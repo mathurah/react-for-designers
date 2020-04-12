a5e637131f98924c3f1e1646e326cabcade984ab
mathurahravigulan <mathurah.ravigulan@edu.uwaterloo.ca>
2020-04-12 12:18:52 -0400

    animations

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,42 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+// component with a state - going to listen to different events that's the lifecycle of the component
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // initial state for the component
+    this.state={
+      hasScrolled: false
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  // handleScroll = {event} => {
+  //   const scrollTop = window.pageYOffset 
+  //   if (scrollTop > 50) {
+  //     this.setState({hasScrolled: true})
+  //   }
+  //   else {
+  //     this.setState({hasScrolled: false})
+  //   }
+  // }
+  render() {
+    return (
+    <div className={this.state.hasScrolled ? 'Header HeaderScrolled':'Header '}>
+      <div className="HeaderGroup">
+         <Link to="/"><img src ={require('../images/logo-designcode.svg')} width ="30"></img></Link>   
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Workshops</Link>
+        <Link to="/buy"><button>Buy</button></Link>
+     </div>
   </div>
-)
-
+    )
+  }
+}
 export default Header
